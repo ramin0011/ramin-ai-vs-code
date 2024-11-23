639c6e9a8496c9e2b864c76de8e747b585c3eb2b
Ramin <Ramin.mh2000@gmail.com>
2024-11-23 14:45:40 +0330

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import axios from "axios";
 import * as vscode from "vscode";
+import https from "https";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "ramincode" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
     "ramincode.helloWorld",
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
       vscode.window.showInformationMessage("Hello World from RaminCode!");
     }
   );
@@ -32,9 +24,15 @@ export function activate(context: vscode.ExtensionContext) {
 
         if (selectedText) {
           try {
-            const response = await axios.post(`https://localhost:7116/ai`, {
-              input: selectedText,
-            });
+            const response = await axios.post(
+              `https://localhost:7116/ai`,
+              { input: selectedText },
+              {
+                httpsAgent: new https.Agent({
+                  rejectUnauthorized: false,
+                }),
+              }
+            );
             vscode.window.showInformationMessage(
               `AI Response: ${response.data}`
             );
@@ -51,7 +49,6 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(aiCommand);
-
   context.subscriptions.push(disposable);
 }
 
